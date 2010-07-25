4993adfce2b0d4b61d7742ea52af4d6caa9ee0f8
Nick Dunn <nicholas.c.dunn@gmail.com>
2010-07-25 01:03:33 -0400

    Adding code to calculate number of adjacent mines

--- Minesweeper.scala
+++ Minesweeper.scala
@@ -42,7 +42,7 @@ class Minefield(width:Int, height:Int, numMines:Int) {
     coords.toList
   }
 
-  private def calculateMineLocations() = {
+  private def calculateMineLocations():Array[Array[Minestatus.Value]] = {
     val mineCoords = pickRandomCoordinates(numMines, 0, 0, width, height)
     // 2d array of Minestatus
     val mines = Array.fill(height,width)(Minestatus.Safe)
@@ -51,7 +51,66 @@ class Minefield(width:Int, height:Int, numMines:Int) {
     mines
   }
   
-  private val mines = calculateMineLocations()
+  private val mines:Array[Array[Minestatus.Value]] = calculateMineLocations()
+  
+
+  // private def subRectArray(array:Array[Array[E]], x:Int, y:Int, width:Int, height:Int) = {
+  //   val numRows = array.length
+  //   val numCols = array(0).length
+  //   // Array.fill(width, height)
+  //   
+  //   null
+  //   
+  //   
+  // }
+
+
+
+  private def calculateNumAdjacentMines():Array[Array[Int]] = {
+    
+    // Simple algorithm.  Loop through all adjacent squares for each square
+    // on the board, and check how many of those are mines.
+    // To avoid special case logic at edges and corners, we extent the board
+    // one square in each direction
+    val scratchBoard = Array.fill(width+2,height+2)(Minestatus.Safe)
+    
+    // Copy the interior section of the board
+    var counter = 0
+    for (row <- mines) {
+      // Skip the first empty row
+      val scratchRow = scratchBoard(counter+1)
+      Array.copy(row, 0, scratchRow, 1, row.length)
+      counter += 1
+    }
+    
+    val dirs = List(-1,0,1)
+    // crossproduct of the different directions you can take; same as manually
+    // calculating the east, southeast, south, southwest, etc
+    val adjacent = (for (x<-dirs; y<-dirs) yield(x,y))
+    
+    // Now loop through and count up
+    val answers = Array.fill(width,height)(0)
+    for (row <- 0 until height) {
+      for (col <- 0 until width) {
+        var counter = 0
+        for ((x,y) <- adjacent) {
+          // Need to add 1 to row and col to make up for the extra space
+          val scratchRow = row + y + 1
+          val scratchCol = col + x + 1
+          if (scratchBoard(scratchRow)(scratchCol) == Minestatus.Dangerous) {
+            counter += 1
+          }
+        }
+        answers(row)(col) = counter
+      }
+    }
+    println(answers.map(_.mkString(" ")).mkString("\n"))
+    answers
+  }
+  
+  val adjacentCounts = calculateNumAdjacentMines()
+
+
   
   override def toString():String = {
     // for all the rows
