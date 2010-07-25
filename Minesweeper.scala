69ee86d91039ff27cc8f6856c589234802a69ab2
Nick Dunn <nicholas.c.dunn@gmail.com>
2010-07-25 00:21:05 -0400

    Adding the mine generation code

--- Minesweeper.scala
+++ Minesweeper.scala
@@ -1,5 +1,5 @@
 import java.util.Random
-
+import scala.collection.mutable.ListBuffer
 object Minesweeper {
 
   
@@ -18,25 +18,51 @@ object Minestatus extends Enumeration {
 
 class Minefield(width:Int, height:Int, numMines:Int) {
   import Minestatus._
-  
-  
   val random = new Random()
   
+  // Calculates random (x,y) pairs in the given range
+  private def pickRandomCoordinates(numCoords:Int, minX:Int, minY:Int, maxX:Int, maxY:Int):List[Tuple2[Int,Int]] = {
+    var coords:ListBuffer[Tuple2[Int,Int]] = new ListBuffer()
+    for (i <- 0 until numCoords) {
+      def randomBetween(lowerBound:Int, upperBound:Int):Int = {
+        val diff = upperBound - lowerBound
+        lowerBound + random.nextInt(diff)
+      }
+      var found = false
+      while (!found) {
+        val x = randomBetween(minX, maxX)
+        val y = randomBetween(minY, maxY)
+        val tuple = (x,y)
+        if (!coords.contains(tuple)) {
+          found = true
+          coords += tuple
+        }
+      }
+    }
+    coords.toList
+  }
+
+  private def calculateMineLocations() = {
+    val mineCoords = pickRandomCoordinates(numMines, 0, 0, width, height)
+    // 2d array of Minestatus
+    val mines = Array.fill(height,width)(Minestatus.Safe)
+    // we have x,y, change to row, column
+    mineCoords.foreach(x => mines(x._2)(x._1) = Minestatus.Dangerous)
+    mines
+  }
   
-  
-  
-  // Start as all empty
-  private val mines = Array.fill(height,width)(Minestatus.Safe)
+  private val mines = calculateMineLocations()
   
   override def toString():String = {
     // for all the rows
     val sb = new StringBuilder()
-    
     for (row <- mines) {
       sb.append(row.map(x => if (x==Minestatus.Safe) { "_" } else { "X" } ).mkString(" "))
       sb.append("\n")
     }
     sb.toString()
   }
+  
+  
 }
 
